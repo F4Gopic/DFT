ef40925d06f3b469a4ae77d902906bfe5bdca819
Zach L <109915621+F4Gopic@users.noreply.github.com>
2023-03-12 16:00:11 -0400

    Added C++ Code To Implement Multiplayer

---
+++ Source/DFT/Private/MP_ActorInitiation.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MP_ActorInitiation.h"
+
+// Sets default values
+AMP_ActorInitiation::AMP_ActorInitiation()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMP_ActorInitiation::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMP_ActorInitiation::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
